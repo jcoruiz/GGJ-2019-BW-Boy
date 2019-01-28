b224ead36542f747b6f88ca485476a9ea822f627
Jonathan Olivares <=>
2019-01-28 15:34:08 -0300

    gitignore

---
+++ Assets/Scripts/TestClass.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewMonoBehaviour : MonoBehaviour
+{
+    public string Test()
+    {
+        return "Funciona";    
+    }
+    
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
